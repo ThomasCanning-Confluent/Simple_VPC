c99f79ec3a4b1bf6b4b97bfd4e9ade8ca8890365
tcaning <tcanning@confluent.io>
2024-06-20 11:10:51 +0100

    Changing bucket size

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 #Resources take a type as the 1st argument (corresponding to an AWS service) and a name to identify it as 2nd argument
 resource "aws_vpc" "main" {
-  #Customise the settings of a resurce within the block
+  #Customise the settings of a resource within the block
   cidr_block = "10.0.0.0/16"
 }
 
@@ -48,7 +48,7 @@ resource "aws_security_group" "public_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 22
+    from_port   = 22 #This specifies SSH as part of the security group
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
@@ -72,7 +72,7 @@ resource "aws_instance" "public_instances" {
   //This adds an encrypted EBS volume to the instance (gp2 is general purpose SSD, 4GB in size)
   root_block_device {
     volume_type           = "gp2"
-    volume_size           = 4
+    volume_size           = 8
     delete_on_termination = true
     encrypted             = true  # Ensures the root EBS volume is encrypted
   }
@@ -84,6 +84,5 @@ resource "aws_instance" "public_instances" {
 
 //Adds a bucket to the AWS account
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "bucket-4324324"
+  bucket = "bucket-tcanning"
 }
-
