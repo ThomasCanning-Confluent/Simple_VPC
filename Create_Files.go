5a30ad3c5ba47f445fbee3b3f33b7a9e3365bdd5
tcaning <tcanning@confluent.io>
2024-06-21 13:14:20 +0100

    Commenting go script

--- Create_Files.go
+++ Create_Files.go
@@ -7,10 +7,13 @@ import (
 "io"
 )
 
+//This GO program creates 3 files with random data of 100MB each
 func main() {
 	for i := 1; i <= 3; i++ {
+		//This line creates a file with the name filei.txt where i takes the values 1, 2, then 3
 		file, _ := os.Create(fmt.Sprintf("file%d.txt", i))
 		defer file.Close()
+		//This line writes 100MB of random data to the file
 		io.CopyN(file, rand.Reader, int64(100) * 1024 * 1024)
 	}
 }
